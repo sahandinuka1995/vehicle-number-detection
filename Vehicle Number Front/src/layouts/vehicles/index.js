cd831e0c9f651d4ddba0561fcdc3aec90187b310
sahandinuka1995@gmail.com <Sahee@123>
2021-12-13 15:54:36 +0530

    initial commit

---
+++ Vehicle Number Front/src/layouts/vehicles/index.js
@@ -0,0 +1,58 @@
+/**
+ * @author Sahan Dinuka
+ * @CreatedBy IntelliJ IDEA
+ * @created 13/12/2021 - 1:01 PM
+ */
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import DataTable from "examples/Tables/DataTable";
+import {vehicleTable} from '../../const/tables'
+
+function Vehicles() {
+    const data = vehicleTable
+
+    return (
+        <DashboardLayout>
+            <DashboardNavbar/>
+            <MDBox pt={6} pb={3}>
+                <Grid container spacing={6}>
+                    <Grid item xs={12}>
+                        <Card>
+                            <MDBox
+                                mx={2}
+                                mt={-3}
+                                py={3}
+                                px={2}
+                                variant="gradient"
+                                bgColor="info"
+                                borderRadius="lg"
+                                coloredShadow="info"
+                            >
+                                <MDTypography variant="h6" color="white">
+                                    All Vehicles Data
+                                </MDTypography>
+                            </MDBox>
+                            <MDBox pt={3}>
+                                <DataTable
+                                    table={data}
+                                    isSorted={false}
+                                    entriesPerPage={false}
+                                    showTotalEntries={false}
+                                    noEndBorder
+                                />
+                            </MDBox>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </MDBox>
+            <Footer/>
+        </DashboardLayout>
+    );
+}
+
+export default Vehicles;
