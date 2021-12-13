8efe704542b6d0ee1af21dada70cb7cbad7a4a4a
sahandinuka1995@gmail.com <Sahee@123>
2021-12-13 15:55:34 +0530

    initial commit

---
+++ Vehicle Number Front/src/const/strings.js
@@ -0,0 +1,15 @@
+/**
+ * @author Sahan Dinuka
+ * @CreatedBy IntelliJ IDEA
+ * @created 12/12/2021 - 12:33 PM
+ */
+export const strings = {
+    // footer
+    copyrightText: `© ${new Date().getFullYear()}, made by Sahan Dinuka.`,
+
+    // dashboard
+    totalVehicles: "Total Vehicles",
+    activeVehicles: "Active Vehicles",
+    unsettleVehicles: "Un-Settle Vehicles"
+}
+
